2f12dc1f956bc401d7d924802fd8b0c371b00330
farhanisty <farhannivtasija@gmail.com>
2025-06-10 15:23:12 +0700

    example inputable

---
+++ src/guigeometri/Inputable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package guigeometri;
+
+import java.util.List;
+
+/**
+ *
+ * @author farhannivta
+ */
+public interface Inputable {
+    public List<String> getInputs();
+}
